71459e62b6bb7e4f49c81884b9adecd3ffcf4461
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-05-15 14:51:15 +0300

    Cell files are modified, moved.

---
+++ PomodomoLight/Cells/InformationTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InformationTableViewCell.swift
+//  PomodomoLight
+//
+//  Created by Asım Altınışık on 13.05.2023.
+//
+
+import UIKit
+
+class InformationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var informationLabel: UILabel!
+    @IBOutlet weak var informationValue: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
